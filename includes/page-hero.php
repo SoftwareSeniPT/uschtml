b3cb836d3e21b6933524820b6ae6f964b991eef0
Ganis Angger Atmawarin <atmawarin@gmail.com>
2016-09-03 13:48:34 +0700

    Change from PHP to KIT

--- includes/page-hero.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $hero_image = (isset($hero_image) ? $hero_image : "./images/Hero-partner-resource.jpg");
-?>
-
-<section class="page-hero" style="background-image: url(<?php echo $hero_image ?>)">
-    <div class="page-hero-wrapper">
-      <img src="./images/white-logo.png" alt="">
-    </div>
-</section>

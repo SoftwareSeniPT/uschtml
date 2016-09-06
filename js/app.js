b65ef920de718a45d509fcfd6f61efc14bcaf902
Rudi Wahyudi <diruuu@yahoo.com>
2016-09-06 17:20:12 +0700

    fixed IE detection

--- js/app.js
+++ js/app.js
@@ -19,12 +19,9 @@ var app = {
         app.objectFitPolyfill();
         // app.checkIfOldIE();
         app.submenuToggle();
-
-
-        // Check IE
-        setTimeout(function () {
-          console.log(jQuery("html").attr("class"));
-        }, 1000);
+        app.browserDetect.init(function(info){
+          console.log("Info", info);
+        });
     },
     submenuToggle: function() {
         function checkIfOpen(elm, notOpen, open) {
@@ -429,6 +426,46 @@ var app = {
                 jQuery('body').removeClass('not-on-top');
             }
         });
+    },
+    browserDetect: {
+        init: function (callback) {
+            this.browser = this.searchString(this.dataBrowser) || "Other";
+            this.version = this.searchVersion(navigator.userAgent) || this.searchVersion(navigator.appVersion) || "Unknown";
+            callback({browser: this.browser, version: this.version});
+        },
+        searchString: function (data) {
+            for (var i = 0; i < data.length; i++) {
+                var dataString = data[i].string;
+                this.versionSearchString = data[i].subString;
+
+                if (dataString.indexOf(data[i].subString) !== -1) {
+                    return data[i].identity;
+                }
+            }
+        },
+        searchVersion: function (dataString) {
+            var index = dataString.indexOf(this.versionSearchString);
+            if (index === -1) {
+                return;
+            }
+
+            var rv = dataString.indexOf("rv:");
+            if (this.versionSearchString === "Trident" && rv !== -1) {
+                return parseFloat(dataString.substring(rv + 3));
+            } else {
+                return parseFloat(dataString.substring(index + this.versionSearchString.length + 1));
+            }
+        },
+        dataBrowser: [
+            {string: navigator.userAgent, subString: "Edge", identity: "MS Edge"},
+            {string: navigator.userAgent, subString: "MSIE", identity: "Explorer"},
+            {string: navigator.userAgent, subString: "Trident", identity: "Explorer"},
+            {string: navigator.userAgent, subString: "Firefox", identity: "Firefox"},
+            {string: navigator.userAgent, subString: "Opera", identity: "Opera"},
+            {string: navigator.userAgent, subString: "OPR", identity: "Opera"},
+            {string: navigator.userAgent, subString: "Chrome", identity: "Chrome"},
+            {string: navigator.userAgent, subString: "Safari", identity: "Safari"}
+        ]
     }
 }
 
